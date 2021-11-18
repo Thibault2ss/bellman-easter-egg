66eba70c0a8b0883ad46765c59b5eda86c7c52b7
Thibault de Saint Sernin <thibault.de-saint-sernin@bellman.immo>
2021-11-18 23:28:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+(function (document, code) {
+  if (!document.addEventListener) {
+    return;
+  }
+
+  var iframeId = "bellman-easter-egg";
+  var html =
+    "https://rawgit.com/Thibault2ss/bellman-easter-egg/master/nyan/index.html";
+  var l = "";
+
+  const removeEasterEgg = () => {
+    const iframe = document.getElementById(iframeId);
+
+    console.log("iframe", iframe);
+    if (iframe) {
+      iframe.remove();
+    }
+  };
+
+  const showEasterEgg = () => {
+    const iframe = document.getElementById(iframeId);
+
+    if (!iframe) {
+      document.body.innerHTML +=
+        '<iframe id="' +
+        iframeId +
+        '" src="' +
+        html +
+        '" style="border:0;width:100vw;height:100vh;position:fixed;top:0;z-index:1337" allowtransparency="true"></iframe>';
+
+      setTimeout(removeEasterEgg, 3000);
+    }
+  };
+
+  const onKeyDown = (event) => {
+    l += event.key;
+    if (l.indexOf(code) !== -1) {
+      l = "";
+      console.log("triggered");
+      showEasterEgg();
+    }
+  };
+
+  document.addEventListener("keydown", onKeyDown);
+})(document, "thib");
